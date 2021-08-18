b6a26b9eaa3a6be0eb54d4673894bdfd76bf849e
bnwlkr <bnwlkr@icloud.com>
2021-08-17 18:40:14 -0700

    Update lactate script with Kimia's changes - to get extracellular lactate concentration

--- lactate/lactate.py
+++ lactate/lactate.py
@@ -4,12 +4,15 @@ import sys
 # https://github.com/thierry3000/VBL/blob/master/src/CellsSystem.cpp#L708
 
 BufCapEnv = 0.584e-3
-def get_AcL(pH_ex, volume_extra):
-    return (7.5443 - pH_ex) * volume_extra * BufCapEnv
+# 89.07 g/mol is the MW of lactate
+# returns extracellular [lactate] in mM
+ 
+def get_AcL(pH_ex):
+    return (7.5443 - pH_ex) * BufCapEnv * 1.0e6 * (1/89.07)
 
 def main():
     with h5py.File(sys.argv[1], 'r') as orig:
-        with h5py.File("out.h5", 'w') as new:
+        with h5py.File("tumor-lac.h5", 'w') as new:
             for a in orig.attrs:
                 new.attrs[a] = orig.attrs[a]
             new.copy(orig['out0540'], new)
@@ -18,13 +21,10 @@ def main():
             new.copy(orig['parameters'], new)
             cells_group = new['out0540']['cells']
             pH_ex = cells_group['pH_ex']
-            volume_extra = new['out0540']['vbl']['volume_extra']
             cells_group.create_dataset('AcL_ex', data=pH_ex)
             AcL_ex = cells_group['AcL_ex']
-            AcL_ex[:] = [get_AcL(pH_ex[i], volume_extra[i]) for i in range(AcL_ex.shape[0])]
-
-
+            AcL_ex[:] = [get_AcL(pH_ex[i]) for i in range(AcL_ex.shape[0])]
 
 if __name__=='__main__':
-    main()
+    sys.exit(main())
 
